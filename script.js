a6b40d2319c1d92b03ac6d26ecef0e3896f641e8
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-08-27 17:15:45 -0700

    Added questions to script.js

--- script.js
+++ script.js
@@ -10,8 +10,16 @@ const quizData = [
         answer: 0
     },
     {
-        question: ""
-    }
+        question: "What is the purpose of the 'console.log()' function in JavaScript?",
+        options: ["To display a message box", "To log errors in the console", "To output data to the browser's console"],
+        answer: 0
+    },
+    {
+        question: "What does the acronym DOM stand for in the context of web development?",
+        options:["Document Object Model", "Data Object Management", "Document Object Mapping","Direct Object Manipulation"],
+        answer: 0
+
+    },
 ];
 
 const quizContainer = document.getElementById("quiz");
@@ -21,41 +29,41 @@ let currentQuestionIndex = 0;
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-  startButton.style.display = "none";
-  quizContainer.style.display = "block";
-  showQuestion();
+    startButton.style.display = "none";
+    quizContainer.style.display = "block";
+    showQuestion();
 }
 
 function showQuestion() {
-  const questionObj = quizData[currentQuestionIndex];
-  quizContainer.innerHTML = `
+    const questionObj = quizData[currentQuestionIndex];
+    quizContainer.innerHTML = `
     <h3>${currentQuestionIndex + 1}. ${questionObj.question}</h3>
     ${generateOptions(questionObj.options)}
     <button id="next-button">Next</button>
   `;
 
-  const nextButton = document.getElementById("next-button");
-  nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < quizData.length) {
-      showQuestion();
-    } else {
-      showResults();
-    }
-  });
+    const nextButton = document.getElementById("next-button");
+    nextButton.addEventListener("click", () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizData.length) {
+            showQuestion();
+        } else {
+            showResults();
+        }
+    });
 }
 
 function generateOptions(options) {
-  let optionsHTML = "";
-  for (let i = 0; i < options.length; i++) {
-    optionsHTML += `
+    let optionsHTML = "";
+    for (let i = 0; i < options.length; i++) {
+        optionsHTML += `
       <input type="radio" name="options" value="${i}" id="option${i}">
       <label for="option${i}">${options[i]}</label><br>
     `;
-  }
-  return optionsHTML;
+    }
+    return optionsHTML;
 }
 
 function showResults() {
-  quizContainer.innerHTML = `<h2>Quiz Results</h2><p>You have completed the quiz!</p>`;
+    quizContainer.innerHTML = `<h2>Quiz Results</h2><p>You have completed the quiz!</p>`;
 }
